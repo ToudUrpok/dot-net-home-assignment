798ab9278b6d22abd6f070ca4e11c3383c73ad1f
y.yakubovich <y.yakubovich@softteco.com>
2024-03-26 12:27:44 +0300

    added AppUser table initialization with test data

--- BlogPlatformBackend/BlogPlatform.Data/BlogContext.cs
+++ BlogPlatformBackend/BlogPlatform.Data/BlogContext.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using BlogPlatform.Data.Entities;
+using System.Reflection.Metadata;
 
 namespace BlogPlatform.Data;
 
@@ -9,4 +10,15 @@ public class BlogContext(DbContextOptions<BlogContext> options)
     public DbSet<AppUser> AppUsers { get; set; }
     public DbSet<Post> Posts { get; set; }
     public DbSet<Comment> Comments { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<AppUser>()
+            .HasData(new AppUser()
+            {
+                Id = 1,
+                UserName = "TestUser",
+                Password = "123456"
+            });
+    }
 }
---
+++ BlogPlatformBackend/BlogPlatform.Data/Migrations/20240326091750_InitAppUserWithTestData.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BlogPlatform.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitAppUserWithTestData : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "AppUsers",
+                columns: new[] { "Id", "Password", "UserName" },
+                values: new object[] { 1, "123456", "TestUser" });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "AppUsers",
+                keyColumn: "Id",
+                keyValue: 1);
+        }
+    }
+}
