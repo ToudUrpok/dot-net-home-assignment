ad039c11be6a87cf5488ae4b0cb01aa75a37d996
y.yakubovich <y.yakubovich@softteco.com>
2024-03-31 11:21:54 +0300

    refactored with the result table variable to make it easier for understanding

--- ComputationalQuestion/CreateAmortizationScheduleRecursiveCTE.sql
+++ ComputationalQuestion/CreateAmortizationScheduleRecursiveCTE.sql
@@ -1,3 +1,5 @@
+
+-- the task input data
 DECLARE 
 	@loan_amount FLOAT = 36000,
 	@loan_term INT = 36,
@@ -6,10 +8,21 @@ DECLARE
 	@recycled_loan_term INT = 48,
 	@recycled_interest_rate FLOAT = .045
 
-DECLARE @monthly_interest FLOAT = @interest_rate / 12
-DECLARE @recycled_monthly_interest FLOAT = @recycled_interest_rate / 12
-DECLARE @monthly_pay FLOAT = @loan_amount * @monthly_interest / ( 1 - POWER(1 + @monthly_interest, - @loan_term) )
+-- variable with the result table where initial and recycled schedules will be inserted
+DECLARE @result_table TABLE
+(
+	payment_number INT,
+	beginning_balance DECIMAL(19,2),
+	monthly_pay DECIMAL(19,2),
+	interest DECIMAL(19,2),
+	principal DECIMAL(19,2),
+	ending_balance DECIMAL(19,2)
+)
 
+-- calculate initial monthly interest and monthly pay
+DECLARE @monthly_interest FLOAT = @interest_rate / 12
+DECLARE @monthly_pay FLOAT = @loan_amount * @monthly_interest / ( 1 - POWER(1 + @monthly_interest, - @loan_term) )
+-- calculate initial amortization schedule
 ;WITH    cte_amortization_schedule
 		(
 			payment_number,
@@ -42,9 +55,29 @@ DECLARE @monthly_pay FLOAT = @loan_amount * @monthly_interest / ( 1 - POWER(1 + 
 			FROM    
 				cte_amortization_schedule
 			WHERE payment_number < @recycle_month
-		),
-		remaining_amount(amount) AS (SELECT MIN(ending_balance) FROM cte_amortization_schedule),
-		cte_amortization_schedule_recycled
+		)
+-- insert initial schedule into result variable
+INSERT @result_table		
+SELECT 
+    payment_number,
+	CONVERT(DECIMAL(19,2), beginning_balance),
+	CONVERT(DECIMAL(19,2), monthly_pay),
+	CONVERT(DECIMAL(19,2), interest),
+	CONVERT(DECIMAL(19,2), principal),
+	CONVERT(DECIMAL(19,2), ending_balance)
+FROM 
+    cte_amortization_schedule
+
+-- detecting remaining amount
+DECLARE @remaining_amount FLOAT
+SELECT @remaining_amount = MIN(ending_balance)
+FROM @result_table
+
+-- calculate recycled monthly interest and monthly pay
+DECLARE @recycled_monthly_interest FLOAT = @recycled_interest_rate / 12
+DECLARE @recycled_monthly_pay FLOAT = @remaining_amount * @recycled_monthly_interest / ( 1 - POWER(1 + @recycled_monthly_interest, - @recycled_loan_term) )
+-- calculate recycled amortization schedule
+;WITH   cte_amortization_schedule_recycled
 		(
 			payment_number,
 			monthly_interest,
@@ -58,11 +91,11 @@ DECLARE @monthly_pay FLOAT = @loan_amount * @monthly_interest / ( 1 - POWER(1 + 
 			SELECT 
 				@recycle_month + 1,
 				@recycled_monthly_interest,
-				(SELECT amount FROM remaining_amount),
-				(SELECT amount FROM remaining_amount) * @recycled_monthly_interest / ( 1 - POWER(1 + @recycled_monthly_interest, - @recycled_loan_term) ),
-				(SELECT amount FROM remaining_amount) * @recycled_monthly_interest,
-				(SELECT amount FROM remaining_amount) * @recycled_monthly_interest / ( 1 - POWER(1 + @recycled_monthly_interest, - @recycled_loan_term) ) - (SELECT amount FROM remaining_amount) * @recycled_monthly_interest,
-				(SELECT amount FROM remaining_amount) - ( (SELECT amount FROM remaining_amount) * @recycled_monthly_interest / ( 1 - POWER(1 + @recycled_monthly_interest, - @recycled_loan_term) ) - (SELECT amount FROM remaining_amount) * @recycled_monthly_interest ) 
+				@remaining_amount,
+				@recycled_monthly_pay,
+				@remaining_amount * @recycled_monthly_interest,
+				@recycled_monthly_pay - @remaining_amount * @recycled_monthly_interest,
+				@remaining_amount - ( @recycled_monthly_pay - @remaining_amount * @recycled_monthly_interest )
 			UNION ALL
 			SELECT    
 				payment_number + 1,
@@ -76,23 +109,24 @@ DECLARE @monthly_pay FLOAT = @loan_amount * @monthly_interest / ( 1 - POWER(1 + 
 				cte_amortization_schedule_recycled
 			WHERE payment_number < @recycled_loan_term + @recycle_month
 		)
-		
+-- insert recycled schedule into result variable
+INSERT @result_table		
 SELECT 
-    payment_number AS [Payment Number],
-	CONVERT(DECIMAL(19,2), beginning_balance) AS [Beginning Balance],
-	CONVERT(DECIMAL(19,2), monthly_pay) AS [Monthly Pay],
-	CONVERT(DECIMAL(19,2), interest) AS [Interest],
-	CONVERT(DECIMAL(19,2), principal) AS [Principal],
-	CONVERT(DECIMAL(19,2), ending_balance) AS [Ending Balance]
-FROM 
-    cte_amortization_schedule
-UNION
-SELECT 
-    payment_number AS [Payment Number],
-	CONVERT(DECIMAL(19,2), beginning_balance) AS [Beginning Balance],
-	CONVERT(DECIMAL(19,2), monthly_pay) AS [Monthly Pay],
-	CONVERT(DECIMAL(19,2), interest) AS [Interest],
-	CONVERT(DECIMAL(19,2), principal) AS [Principal],
-	CONVERT(DECIMAL(19,2), ending_balance) AS [Ending Balance]
+    payment_number,
+	CONVERT(DECIMAL(19,2), beginning_balance),
+	CONVERT(DECIMAL(19,2), monthly_pay),
+	CONVERT(DECIMAL(19,2), interest),
+	CONVERT(DECIMAL(19,2), principal),
+	CONVERT(DECIMAL(19,2), ending_balance)
 FROM 
     cte_amortization_schedule_recycled
+
+-- select the result
+SELECT
+	payment_number AS [Payment Number],
+	beginning_balance AS [Beginning Balance],
+	monthly_pay AS [Monthly Pay],
+	interest AS [Interest],
+	principal AS [Principal],
+	ending_balance AS [Ending Balance]
+FROM @result_table
